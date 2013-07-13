d2a46ff18c584acc7c5db64959edf0f310f76059
David Grudl <david@grudl.com>
2013-07-13 21:34:17 +0200

    added Nette\Tester

---
+++ tests/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Test initialization and helpers.
+ */
+
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
+}
+
+
+// configure environment
+Tester\Helpers::setup();
+class_alias('Tester\Assert', 'Assert');
+date_default_timezone_set('Europe/Prague');
+$_GET = $_POST = $_COOKIE = array();
+Texy::$advertisingNotice = FALSE;
+
+
+if (extension_loaded('xdebug')) {
+	xdebug_disable();
+	Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
+}
+
+
+function test(\Closure $function)
+{
+	$function();
+}
