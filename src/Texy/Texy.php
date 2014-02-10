67d02cd95e4aaa7dae96b24a7d04ba924d641015
David Grudl <david@grudl.com>
2014-02-10 02:06:42 +0100

    Released version 2.4

--- src/Texy/Texy.php
+++ src/Texy/Texy.php
@@ -23,8 +23,8 @@ class Texy extends TexyObject
 	const NONE = FALSE;
 
 	// Texy version
-	const VERSION = '2.3';
-	const REVISION = '$WCREV$ released on $WCDATE$';
+	const VERSION = '2.4';
+	const REVISION = 'released on 2014-02-10';
 
 	// types of protection marks
 	const CONTENT_MARKUP = "\x17";
