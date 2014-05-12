7cb4a7dce0384e64d756c75940a59cd805f12223
David Grudl <david@grudl.com>
2014-05-12 16:48:13 +0200

    TypographyModule: 1 × 2 preserve spaces [Closes #3]

--- src/Texy/modules/TexyTypographyModule.php
+++ src/Texy/modules/TexyTypographyModule.php
@@ -90,7 +90,8 @@ final class TexyTypographyModule extends TexyModule
 			'# <-{1,}#'                               => " \xe2\x86\x90 ",                 // left arrow <--
 			'#={1,}> #'                               => " \xe2\x87\x92 ",                 // right arrow ==>
 			'#\\+-#'                                  => "\xc2\xb1",                       // +-
-			'#(\d++)( ?)x\\2(?=\d)#'                  => "\$1\xc3\x97",                    // dimension sign 123 x 123...
+			'#(\d++) x (?=\d)#'                       => "\$1\xc2\xa0\xc3\x97\xc2\xa0",    // dimension sign 123 x 123...
+			'#(\d++)x(?=\d)#'                         => "\$1\xc3\x97",                    // dimension sign 123x123...
 			'#(?<=\d)x(?= |,|.|$)#m'                  => "\xc3\x97",                       // dimension sign 123x
 			'#(\S ?)\(TM\)#i'                         => "\$1\xe2\x84\xa2",                // trademark (TM)
 			'#(\S ?)\(R\)#i'                          => "\$1\xc2\xae",                    // registered (R)
