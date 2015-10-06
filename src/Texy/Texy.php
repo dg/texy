bfb2c910db5b3db45b0634fa08b8d628db473996
David Grudl <david@grudl.com>
2015-10-06 15:15:50 +0200

    Released version 2.7.1

--- src/Texy/Texy.php
+++ src/Texy/Texy.php
@@ -21,8 +21,8 @@ class Texy extends TexyObject
 	const NONE = FALSE;
 
 	// Texy version
-	const VERSION = '2.7';
-	const REVISION = 'released on 2015-10-04';
+	const VERSION = '2.7.1';
+	const REVISION = 'released on 2015-10-06';
 
 	// types of protection marks
 	const CONTENT_MARKUP = "\x17";
