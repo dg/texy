e21cbfcff0a542cf691082df7427911495524ded
David Grudl <david@grudl.com>
2015-11-07 23:15:56 +0100

    Released version 2.8.2

--- src/Texy/Texy.php
+++ src/Texy/Texy.php
@@ -25,8 +25,8 @@ class Texy
 	const NONE = FALSE;
 
 	// Texy version
-	const VERSION = '2.8.1';
-	const REVISION = 'released on 2015-10-25';
+	const VERSION = '2.8.2';
+	const REVISION = 'released on 2015-11-07';
 
 	// types of protection marks
 	const CONTENT_MARKUP = "\x17";
