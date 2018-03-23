c0b04adc95dac134ae1c9f06402d904fa0f44a23
David Grudl <david@grudl.com>
2018-03-23 11:55:52 +0100

    loader: uses only Composer's autoloader

--- src/texy.php
+++ src/texy.php
@@ -12,41 +12,7 @@ if (PHP_VERSION_ID < 50404) {
 }
 
 spl_autoload_register(function ($class) {
-	static $map = [
-		'Texy\Texy' => 'Texy.php',
-		'Texy\BlockParser' => 'BlockParser.php',
-		'Texy\Configurator' => 'Configurator.php',
-		'Texy\HandlerInvocation' => 'HandlerInvocation.php',
-		'Texy\Helpers' => 'Helpers.php',
-		'Texy\HtmlElement' => 'HtmlElement.php',
-		'Texy\LineParser' => 'LineParser.php',
-		'Texy\Modifier' => 'Modifier.php',
-		'Texy\Module' => 'Module.php',
-		'Texy\Parser' => 'Parser.php',
-		'Texy\Patterns' => 'Patterns.php',
-		'Texy\Regexp' => 'Regexp.php',
-		'Texy\Strict' => 'Strict.php',
-		'Texy\Modules\BlockModule' => 'Modules/BlockModule.php',
-		'Texy\Modules\BlockQuoteModule' => 'Modules/BlockQuoteModule.php',
-		'Texy\Modules\EmoticonModule' => 'Modules/EmoticonModule.php',
-		'Texy\Modules\FigureModule' => 'Modules/FigureModule.php',
-		'Texy\Modules\HeadingModule' => 'Modules/HeadingModule.php',
-		'Texy\Modules\HorizLineModule' => 'Modules/HorizLineModule.php',
-		'Texy\Modules\HtmlModule' => 'Modules/HtmlModule.php',
-		'Texy\Modules\HtmlOutputModule' => 'Modules/HtmlOutputModule.php',
-		'Texy\Image' => 'Image.php',
-		'Texy\Modules\ImageModule' => 'Modules/ImageModule.php',
-		'Texy\Link' => 'Link.php',
-		'Texy\Modules\LinkModule' => 'Modules/LinkModule.php',
-		'Texy\Modules\ListModule' => 'Modules/ListModule.php',
-		'Texy\Modules\LongWordsModule' => 'Modules/LongWordsModule.php',
-		'Texy\Modules\ParagraphModule' => 'Modules/ParagraphModule.php',
-		'Texy\Modules\PhraseModule' => 'Modules/PhraseModule.php',
-		'Texy\Modules\ScriptModule' => 'Modules/ScriptModule.php',
-		'Texy\Modules\TableCellElement' => 'Modules/TableCellElement.php',
-		'Texy\Modules\TableModule' => 'Modules/TableModule.php',
-		'Texy\Modules\TypographyModule' => 'Modules/TypographyModule.php',
-	], $old2new = [
+	static $old2new = [
 		'Texy' => 'Texy\Texy',
 		'TexyConfigurator' => 'Texy\Configurator',
 		'TexyHtml' => 'Texy\HtmlElement',
@@ -59,9 +25,7 @@ spl_autoload_register(function ($class) {
 		'TexyLongWordsModule' => 'Texy\Modules\LongWordsModule',
 		'TexyTypographyModule' => 'Texy\Modules\TypographyModule',
 	];
-	if (isset($map[$class])) {
-		require __DIR__ . '/Texy/' . $map[$class];
-	} elseif (isset($old2new[$class])) {
+	if (isset($old2new[$class])) {
 		class_alias($old2new[$class], $class);
 	}
 });
