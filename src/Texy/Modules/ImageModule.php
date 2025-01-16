dff5a292973404cae3d4464f83f5ad3ff424395b
David Grudl <david@grudl.com>
2025-01-16 08:54:01 +0100

    Regexp: always run in unicode (BC break)

--- src/Texy/Modules/ImageModule.php
+++ src/Texy/Modules/ImageModule.php
@@ -54,7 +54,7 @@ final class ImageModule extends Texy\Module
 		$texy->registerLinePattern(
 			$this->patternImage(...),
 			'#\[\*\ *+([^\n' . Patterns::MARK . ']{1,1000})' . Patterns::MODIFIER . '?\ *+(\*|(?<!<)>|<)\]' // [* urls .(title)[class]{style} >]
-			. '(?::(' . Patterns::LINK_URL . '|:))??()#Uu',
+			. '(?::(' . Patterns::LINK_URL . '|:))??()#U',
 			'image',
 		);
 	}
@@ -69,7 +69,7 @@ final class ImageModule extends Texy\Module
 			// [*image*]: urls .(title)[class]{style}
 			$text = Texy\Regexp::replace(
 				$text,
-				'#^\[\*([^\n]{1,100})\*\]:[\ \t]+(.{1,1000})[\ \t]*' . Patterns::MODIFIER . '?\s*()$#mUu',
+				'#^\[\*([^\n]{1,100})\*\]:[\ \t]+(.{1,1000})[\ \t]*' . Patterns::MODIFIER . '?\s*()$#mU',
 				$this->patternReferenceDef(...),
 			);
 		}
