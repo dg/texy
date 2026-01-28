5b900d421c91c4060fb9dcde43ef54327a9b3b15
David Grudl <david@grudl.com>
2026-01-29 00:55:25 +0100

    EmoticonModule: removed support for files (BC break)

--- examples/emoticons/demo.php
+++ examples/emoticons/demo.php
@@ -40,8 +40,8 @@ $texy->allowed['emoticon'] = true;
 $texy->emoticonModule->class = 'smilie';
 
 // Add your own custom emoticon
-// When someone types :oops: in the text, it will show redface.gif
-$texy->emoticonModule->icons[':oops:'] = 'redface.gif';
+// When someone types :oops: in the text, it will show this emoji
+$texy->emoticonModule->icons[':oops:'] = '🙈';
 
 
 // Process the text
